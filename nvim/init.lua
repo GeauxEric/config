885f3eda43fe37c405f9d7d80b1972c256e0c8f0
Yun Ding <yunding.eric@gmail.com>
2026-04-09 10:13:50 -0700

    add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,65 @@
+-- Leader
+vim.g.mapleader = " "
+
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Built-in treesitter highlighting (nvim 0.10+)
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function() pcall(vim.treesitter.start) end,
+})
+
+-- Plugins
+require("lazy").setup({
+  -- File search (telescope)
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            find_command = { "find", ".", "-type", "f" },
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>fe", vim.cmd.Explore,    { desc = "Explore (netrw)" })
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Find buffers" })
+    end,
+  },
+
+  -- Keybinding hints
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  -- Colorscheme
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme tokyonight-night")
+    end,
+  },
+})
