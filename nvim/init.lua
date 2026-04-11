52c44f9f7528320bdad413025776ff370f1b4373
GeauxEric <yunding.eric@gmail.com>
2026-04-11 15:04:46 -0700

    Change colorscheme from tokyonight to catppuccin

--- nvim/init.lua
+++ nvim/init.lua
@@ -61,10 +61,11 @@ require("lazy").setup({
 
   -- Colorscheme
   {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme tokyonight-night")
+      vim.cmd("colorscheme catppuccin-frappe")
     end,
   },
 })
